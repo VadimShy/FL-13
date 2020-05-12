ca1220323fdec0b8ea0cf88b968bc16aa81ef564
VadimShy <vadim.syu@gmail.com>
2020-05-12 23:01:57 +0300

    add HW11

---
+++ FL13_HW11/homework/app.js
@@ -0,0 +1,58 @@
+const data = [
+  {
+    'folder': true,
+    'title': 'Pictures',
+    'children': [
+      {
+        'title': 'logo.png'
+      },
+      {
+        'folder': true,
+        'title': 'Vacations',
+        'children': [
+          {
+            'title': 'spain.jpeg'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    'folder': true,
+    'title': 'Desktop',
+    'children': [
+      {
+        'folder': true,
+        'title': 'screenshots',
+        'children': null
+      }
+    ]
+  },
+  {
+    'folder': true,
+    'title': 'Downloads',
+    'children': [
+      {
+        'folder': true,
+        'title': 'JS',
+        'children': null
+      },
+      {
+        'title': 'nvm-setup.exe'
+      },
+      {
+        'title': 'node.exe'
+      }
+    ]
+  },
+  {
+    'title': 'credentials.txt'
+  }
+];
+
+const rootNode = document.getElementById('root');
+
+function createTree(rootNode, obj) {
+  rootNode.append(createTreeDom(obj));
+}
+
